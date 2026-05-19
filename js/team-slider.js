b5fe32eb114a6dabd29b85a590d336b46d619635
SerhiiKharyponcuk <hariponcuksergej@gmail.com>
2026-05-19 17:57:58 +0200

    slider

--- js/team-slider.js
+++ js/team-slider.js
@@ -1,1 +1,30 @@
+const arrow_btn_left = document.querySelector('.left');
+const arrow_btn_right = document.querySelector('.right');
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
 
+function changeSlides() {
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+    slides[currentSlide].style.display = 'flex';
+    slides[currentSlide].style.transform = 'scale(1)';
+}
+changeSlides();
+slides[currentSlide].style.display = 'flex';
+
+arrow_btn_left.addEventListener('click', () => {
+    changeSlides();
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+});
+
+arrow_btn_right.addEventListener('click', () => {
+    changeSlides();
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+});
