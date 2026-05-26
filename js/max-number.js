d981512862ba63e3c66a5c6db6475222586f6969
SerhiiKharyponcuk <hariponcuksergej@gmail.com>
2026-05-26 18:14:41 +0200

    max-namber

--- js/max-number.js
+++ js/max-number.js
@@ -1,1 +1,65 @@
+const maxNumber1 = document.getElementById('max-num1');
+const maxNumber2 = document.getElementById('max-num2');
+const maxNumber3 = document.getElementById('max-num3');
+const maxNumberResult = document.getElementById('max-number');
+const ctx = document.getElementById('myChart');
+let myChart = null;
+
+maxNumber1.addEventListener('input', findMax);
+maxNumber2.addEventListener('input', findMax);
+maxNumber3.addEventListener('input', findMax);
+
+
+function findMax() {
+    const num1 = parseFloat(maxNumber1.value);
+    const num2 = parseFloat(maxNumber2.value);
+    const num3 = parseFloat(maxNumber3.value);
+
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+        console.log('Please enter valid numbers in all fields.');
+        return;
+    }
+
+    const maxNum = Math.max(num1, num2, num3);
+    maxNumberResult.textContent = maxNum;
+
+    if (myChart) {
+        myChart.destroy();
+    }
+
+myChart =
+      new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['1', '2', '3'],
+      datasets: [{
+        label: 'Графік введених чисел',
+        data: [num1, num2, num3],
+        borderWidth: 1,
+          backgroundColor: [
+      'rgba(255, 99, 132, 0.6)',
+      'rgba(255, 159, 64, 0.6)',
+      'rgba(54, 162, 235 , 0.6)',
+    ],
+    borderColor: [
+      'rgb(75, 192, 192)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 205, 86)',
+    ],
+    borderWidth: 5
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+  if (ctx.parentElement.hidden) {
+    ctx.parentElement.hidden = false;
+  }
+}
+
 
--- js/number-guess.js
+++ js/number-guess.js
@@ -1,1 +1,57 @@
+const input_number_guess = document.querySelector(".input_number_guess");
+const submit_number_guess = document.querySelector(".number_guess_search_btn");
+const count_attempts = document.querySelector(".count_attempts");
+const text_number_guess = document.querySelector("#text_number_guess");
+let attempts = 3;
+let computer_choise =  computer_guess();
+console.log(computer_choise);
+submit_number_guess.addEventListener("click", function() {
+    if (attempts > 0) {
+        const user_guess = number_guess();
+        if (user_win(user_guess, computer_choise)) {
+            text_number_guess.textContent = "Congratulations! You've guessed the correct number!";
+            text_number_guess.classList.remove("red");
+            text_number_guess.classList.add("green");
+            attempts = 0; // End the game
+             update_attempts(attempts);
+        } else if (user_guess < computer_choise) {
+            text_number_guess.textContent = "Too low! Try again.";
+            text_number_guess.classList.remove("green");
+            text_number_guess.classList.add("red");
+            attempts--;
+             update_attempts(attempts);
+        } else {
+            text_number_guess.textContent = "Too high! Try again.";
+            text_number_guess.classList.remove("green");
+            text_number_guess.classList.add("red");
+            attempts--;
+             update_attempts(attempts);
+        }
+    } else {
+        attempts = 3; 
+         update_attempts(attempts);
+        // Reset attempts for a new game
+        computer_choise = computer_guess(); // Generate a new random number
+    //    Game over! Starting a new game.
+    console.log(computer_choise);
+    }
+});
 
+function computer_guess() {
+    return Math.floor(Math.random() * 11);
+}
+
+function number_guess() {
+    const user_guess = parseInt(input_number_guess.value);
+    if (isNaN(user_guess) || user_guess < 0 || user_guess > 10) {
+        return;//все пропало
+    }
+    return user_guess;
+}  
+function user_win(user_guess, computer_number) {
+    return user_guess === computer_number;
+}
+function update_attempts(attempts) {
+    console.log(attempts);
+    count_attempts.textContent = `${attempts}`;
+}
